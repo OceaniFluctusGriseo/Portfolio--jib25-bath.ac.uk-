d65c22e3bef8477ec3a6955870ad068b59d04cb9
Jay Malhotra <jay0@me.com>
2020-07-15 01:09:47 +0100

    Found a copy of your RPG
    Don't ask me how I had it

---
+++ Personal/fireemblem.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FireEmblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You encounter Reinhardt.");
+            Console.WriteLine("He attacks you with Dire Thunder, and lands two critical hits.");
+            Console.WriteLine("You are dead. Game over.")
+        }
+    }
+}
